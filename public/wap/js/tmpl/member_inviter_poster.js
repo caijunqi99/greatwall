5ba27ea458f7a2999a1a6abaf2e0efe62eed85d5
caijunqi <604840631@qq.com>
2019-12-18 00:55:45 +0800

    cai

--- public/wap/js/tmpl/member_inviter_poster.js
+++ public/wap/js/tmpl/member_inviter_poster.js
@@ -1,5 +1,6 @@
+
 $(function() {
-    var e = getCookie("key");
+    var e = getParam('key');
     $.getJSON(ApiUrl + "/memberinviter/index.html?key="+e, function(e) {
         //checkLogin(e.login);
         if (e.result.refer_qrcode_logo == null) {
