bc0272fb6f53e7d2b277888b55b63cef11fd1aee
chenzuijianghu <chenzuijianghu@163.com>
2019-12-26 17:21:13 +0800

    s

--- application/mobile/controller/Goods.php
+++ application/mobile/controller/Goods.php
@@ -24,6 +24,9 @@ class Goods extends MobileMall {
     public function getRandGoods(){
         $model_goods = Model('goods');
         $result  = $model_goods->getRandGoods();
+        foreach ($result as $key => $value) {
+            $result[$key]['goods_image_url'] = goods_cthumb($value['goods_image'], 360, $value['store_id']);
+        }
         output_data($result);
     }
 
