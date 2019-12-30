47f87c05df2c97ea24bc7db1320389d9fe9425be
caijunqi <604840631@qq.com>
2019-12-30 15:19:06 +0800

    cai

--- public/wap/js/tmpl/member_inviter_poster.js
+++ public/wap/js/tmpl/member_inviter_poster.js
@@ -1,6 +1,15 @@
 
 $(function() {
-    var e = getCookie("key");
+    var url = location.search; 
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for(var i = 0; i < strs.length; i ++) {
+            theRequest[strs[i].split("=")[0]]=(strs[i].split("=")[1]);
+        }
+    }
+    var e = theRequest['key'];
     $.getJSON(ApiUrl + "/memberinviter/index.html?key="+e, function(e) {
         //checkLogin(e.login);
         if (e.result.refer_qrcode_logo == null) {
