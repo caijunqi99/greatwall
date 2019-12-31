1f4607be517b83958e0da0d42a0febcbb2115e98
chenzuijianghu <chenzuijianghu@163.com>
2019-12-31 18:45:01 +0800

    s

--- application/mobile/controller/Memberauth.php
+++ application/mobile/controller/Memberauth.php
@@ -23,13 +23,14 @@ class Memberauth extends MobileMall
         $member_id = input('param.member_id');
         $commit = input('param.commit');
         if (empty($member_id)) {
-            output_error('member_id参数有误');
+            output_error('参数有误');
         }
         $member_model = model('member');
         $memberbank_model = model('memberbank');
         $condition = array();
         $condition['member_id'] = $member_id;
         $member_info = $member_model->getMemberInfo($condition);
+        if (!$member_info) output_error('没有此用户信息！');
         if ($commit != 1) {
             
             $bankinfo = $memberbank_model -> getMemberbankInfo($condition);
@@ -51,61 +52,62 @@ class Memberauth extends MobileMall
             );
             output_data($logindata);
         }else{
-            $idcard = input('post.idcard');
-            if (!is_idcard($idcard)) {
-                output_error('请输入正确的身份证号码！');
-            }
-            $member_array = array(
-                "member_truename"   => $member_info['member_truename']?$member_info['member_truename']:input('post.username'),
-                "member_idcard"     => input('post.idcard'),
-                "member_provinceid" => input('post.member_provinceid'),
-                "member_cityid"     => input('post.member_cityid'),
-                "member_areaid"     => input('post.member_areaid'),
-                "member_townid"     => input('post.member_townid'),
-                "member_villageid"  => input('post.member_villageid'),
-                "member_areainfo"   => input('post.member_areainfo'),
-                "member_auth_state" => $member_info['member_auth_state']==0?1:$member_info['member_auth_state'],
-            );
-            // $writeLog = [
-            //     'input' =>input(),
-            //     '_FILES' =>$_FILES
-            // ];
-            // Log::write($writeLog);
-            // 
-            //上传身份证图
-            if ($_FILES) {
-                $files['member_idcard_image2'] = isset($_FILES['member_idcard_image2']['name'])?$_FILES['member_idcard_image2']['name']:'';
-                $files['member_idcard_image3'] = isset($_FILES['member_idcard_image3']['name'])?$_FILES['member_idcard_image3']['name']:'';
-                $upload = $this->img($files,$member_id);
-                if (!empty($upload['member_idcard_image2'])) {
-                    $member_array['member_idcard_image2'] = $upload['member_idcard_image2'];
+            if ($member_info['member_auth_state']==0 || $member_info['member_auth_state']==2) {
+                $idcard = input('post.idcard');
+                if (!is_idcard($idcard)) {
+                    output_error('请输入正确的身份证号码！');
                 }
-                if (!empty($upload['member_idcard_image3'])) {
-                    $member_array['member_idcard_image3'] = $upload['member_idcard_image3'];
-                }
-            }
-            $result = $member_model->editMember(['member_id'=>$member_id],$member_array);
-            if($result){
-                $bank_array = array(
-                    'memberbank_type' => "bank",
-                    "memberbank_truename" => input('post.username'),
-                    'memberbank_name' => input('post.member_bankname'),
-                    'memberbank_no' => input('post.member_bankcard'),
-                    'member_id' => $member_id
+                //修改个人信息
+                $member_array = array(
+                    "member_truename"   => $member_info['member_truename']?$member_info['member_truename']:input('post.username'),
+                    "member_idcard"     => $idcard,
+                    "member_provinceid" => input('post.member_provinceid'),
+                    "member_cityid"     => input('post.member_cityid'),
+                    "member_areaid"     => input('post.member_areaid'),
+                    "member_townid"     => input('post.member_townid'),
+                    "member_villageid"  => input('post.member_villageid'),
+                    "member_areainfo"   => input('post.member_areainfo'),
+                    "member_auth_state" => 1,
                 );
-                $ret = $memberbank_model->addMemberbank($bank_array);
-                if($ret){
-                    $logindata = array(
-                         'member_id' => $member_id
+                //上传身份证图
+                if ($_FILES) {
+                    $files['member_idcard_image2'] = isset($_FILES['member_idcard_image2']['name'])?$_FILES['member_idcard_image2']['name']:'';
+                    $files['member_idcard_image3'] = isset($_FILES['member_idcard_image3']['name'])?$_FILES['member_idcard_image3']['name']:'';
+                    $upload = $this->img($files,$member_id);
+                    if (!empty($upload['member_idcard_image2'])) {
+                        $member_array['member_idcard_image2'] = $upload['member_idcard_image2'];
+                    }
+                    if (!empty($upload['member_idcard_image3'])) {
+                        $member_array['member_idcard_image3'] = $upload['member_idcard_image3'];
+                    }
+                }
+                //编辑个人信息
+                $result = $member_model->editMember(['member_id'=>$member_id],$member_array);
+                if($result){
+                    $bank_array = array(
+                        'memberbank_type' => "bank",
+                        "memberbank_truename" => input('post.username'),
+                        'memberbank_name' => input('post.member_bankname'),
+                        'memberbank_no' => input('post.member_bankcard'),
+                        'member_id' => $member_id
                     );
-                    output_data($logindata);
+                    $ret = $memberbank_model->addMemberbank($bank_array);
+                    if($ret){
+                        $logindata = array(
+                             'member_id' => $member_id
+                        );
+                        output_data($logindata);
+                    }else{
+                        output_error('提交认证失败');
+                    }
                 }else{
                     output_error('提交认证失败');
                 }
+            }elseif ($member_info['member_auth_state']==1) {
+                output_error('信息审核中！！！');
             }else{
-                output_error('提交认证失败');
+                output_error('已通过实名信息认证，不能重复提交！');
             }
-            
         }
 
     }
