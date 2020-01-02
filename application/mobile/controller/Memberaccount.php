c8b59a6daee6bbe309e253c8ea3223fd2b457392
chenzuijianghu <chenzuijianghu@163.com>
2020-01-02 18:38:30 +0800

    s

--- application/mobile/controller/Memberaccount.php
+++ application/mobile/controller/Memberaccount.php
@@ -419,9 +419,9 @@ class Memberaccount extends MobileMember
         }
         $update = $model_member->editMember(array('member_id' => $this->member_info['member_id']), array('member_paypwd' => md5($data['password'])));
         $message = $update ? lang('password_set_successfully') : lang('password_setting_failed');
-        
+        output_data(['state'=>$update==1?'true':'false','msg'=>$message]);
         if ($update) {
-            output_data(['state'=>$update,'msg'=>$message]);
+            
         } else {
             output_data($message);
         }
